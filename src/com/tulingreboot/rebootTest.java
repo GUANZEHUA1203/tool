ef48fef9a06bbab70221322971f94f2981714309
GUANZEHUA1203 <zehua951203@gmail.com>
2018-04-17 11:03:22 +0800

    first commit

---
+++ src/com/tulingreboot/rebootTest.java
@@ -0,0 +1,69 @@
+package com.tulingreboot;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 
+ */
+
+/**
+ * @author zehua
+ *
+ */
+//图灵机器人测试
+public class rebootTest {
+	public static String getResult(String msg) throws IOException{
+		try {
+			
+			String keys[]={"64f6c01591294327960fbedfc2a83f17","4dfd2816872b4607af2f3834f1ad0f21","da4ebfd925cf4b98b21630eec91a5eff","716e2562950a4da19b8079889a97b530"};
+			int i=new Random().nextInt(4);
+			if(i>3||i<0){
+				System.out.println("****************"+i);
+			}
+			String key=Arrays.asList(keys).get(i);
+			String APIKEY=URLEncoder.encode(key, "UTF-8");
+			URL url = new URL("http://www.tuling123.com/openapi/api?key=" + APIKEY + "&info=" + msg);
+			System.out.println(url);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 	
+			
+			 connection.connect();
+			InputStream  inputStream = connection.getInputStream();
+			
+			InputStreamReader isr=new InputStreamReader(inputStream, "UTF-8");
+			
+			BufferedReader br=new BufferedReader(isr);
+			
+			StringBuffer sb=new StringBuffer();
+			String line="";
+			while ((line=br.readLine())!=null) {
+				sb.append(line);
+			}
+			
+			System.out.println(sb);
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		List<String> question=new ArrayList<String>();
+		question.add("你好");
+		while (true) {
+			getResult("你好,你叫什么名字？");
+		}
+		
+	}
+
+}
